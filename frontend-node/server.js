bb4fe26db813c61db164b2df0859bcea52838984
Muhammad Sannan <61903952+msannan@users.noreply.github.com>
2021-11-09 11:00:21 +0500

    fixed colon issue

--- frontend-node/server.js
+++ frontend-node/server.js
@@ -20,7 +20,7 @@ app.get('/instructor/:id', (req, res) => {
   request(String(`http://backend:8080/instructor/${id}`), function (error, response, body) {
     if (!error && response.statusCode == 200) {
       var obj = JSON.parse(body);
-      name = "Hello "+obj.firstName + " " + obj.lastName + " , from Backend. Thank You for being a great DevOps Teacher!
+      name = "Hello "+obj.firstName + " " + obj.lastName + " , from Backend. Thank You for being a great DevOps Teacher!"
       res.send(name);
     }
   })
