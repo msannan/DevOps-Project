60fc3a1c3f84f3db1927fa9bee66feb4a6dd92e5
Muhammad Sannan <61903952+msannan@users.noreply.github.com>
2021-11-09 10:52:36 +0500

    changed "backend-app:8080" to "backend:8080"

--- frontend-node/server.js
+++ frontend-node/server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/hello', (req, res) => {
-  res.send('Hello Ali! If you see this means frontend is running!\n');
+  res.send('Hello Ali! If you see this means frontend is running! Do /instructor/1 or /student/1 for more :)\n');
 });
 
 var request = require('request');
@@ -20,7 +20,7 @@ app.get('/instructor/:id', (req, res) => {
   request(String(`http://backend:8080/instructor/${id}`), function (error, response, body) {
     if (!error && response.statusCode == 200) {
       var obj = JSON.parse(body);
-      name = "Hello "+obj.firstName + " " + obj.lastName
+      name = "Hello "+obj.firstName + " " + obj.lastName + " , from Backend. Thank You for being a great DevOps Teacher!
       res.send(name);
     }
   })
